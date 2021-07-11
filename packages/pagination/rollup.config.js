5eef6a5415446025a0d55fdf49b17c1acb9c1a12
niconiahi <nicolas.accetta@gmail.com>
2021-07-11 01:25:43 -0300

    fix: update linting commands

--- packages/pagination/rollup.config.js
+++ packages/pagination/rollup.config.js
@@ -1,15 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
 
-import babel from "@rollup/plugin-babel";
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
+import babel from '@rollup/plugin-babel'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import typescript from 'rollup-plugin-typescript2'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import { terser } from 'rollup-plugin-terser'
 
-const PACKAGE_NAME = "index";
-const ENTRY_FILE = "./src/index.ts";
-const OUTPUT_DIR = "./dist";
+const PACKAGE_NAME = 'index'
+const ENTRY_FILE = './src/index.ts'
+const OUTPUT_DIR = './dist'
 
 export default [
   {
@@ -17,10 +17,10 @@ export default [
     output: [
       {
         file: `${OUTPUT_DIR}/${PACKAGE_NAME}.js`,
-        format: "cjs",
-        exports: "named",
-        name: PACKAGE_NAME,
-      },
+        format: 'cjs',
+        exports: 'named',
+        name: PACKAGE_NAME
+      }
     ],
     plugins: [
       peerDepsExternal(),
@@ -28,7 +28,7 @@ export default [
       babel({ inputSourceMap: true }),
       commonjs(),
       terser(),
-      typescript(),
-    ],
-  },
-];
+      typescript()
+    ]
+  }
+]
