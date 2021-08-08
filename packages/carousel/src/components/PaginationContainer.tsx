142a2e23c986a09805b8c9e44d888a9e42ca64d9
niconiahi <nicolas.accetta@gmail.com>
2021-08-08 19:20:21 -0300

    chore(carousel): remove package

--- packages/carousel/src/components/PaginationContainer.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react'
-import { FlexProps, Flex } from '@chakra-ui/react'
-
-export const PaginationContainer: FC<FlexProps> = ({ children, ...flexProps }) => (
-  <Flex className='pagination-container' aria-label='pagination navigation' as='nav' {...flexProps}>
-    {children}
-  </Flex>
-)
--- packages/carousel/src/lib/hooks/usePaginationContext.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from 'react'
-
-// lib
-import { PaginationContext, PaginationContextValues } from '../providers/PaginationProvider'
-
-export const usePaginationContext = (): PaginationContextValues => {
-  return useContext(PaginationContext)
-}
